acd0b4b3c15b6592ff80bdd6b118575c28530b7c
x993 <68646455+x993@users.noreply.github.com>
2022-05-21 10:18:11 -0600

    Back to original

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,52 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import DataTable from "react-data-table-component";
-import Card from "@material-ui/core/Card";
-import SortIcon from "@material-ui/icons/ArrowDownward";
-import movies from "./movies";
+import React from 'react'
 
-const columns = [
-  {
-    id: 1,
-    name: "Title",
-    selector: (row) => row.title,
-    sortable: true,
-    reorder: true
-  },
-  {
-    id: 2,
-    name: "Director",
-    selector: (row) => row.director,
-    sortable: true,
-    reorder: true
-  },
-  {
-    id: 3,
-    name: "Runtime (m)",
-    selector: (row) => row.runtime,
-    sortable: true,
-    right: true,
-    reorder: true
-  }
-];
-
-function App() {
-  return (
-    <div className="App">
-      <Card>
-        <DataTable
-          title="Movies"
-          columns={columns}
-          data={movies}
-          defaultSortFieldId={1}
-          sortIcon={<SortIcon />}
-          pagination
-          selectableRows
-        />
-      </Card>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+export default () => (
+  <div>
+    <h1>This is what we're all about.</h1>
+    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
+  </div>
+)
