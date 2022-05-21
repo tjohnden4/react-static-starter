2d979ec9d3143b8208504c136212038e163a9dc8
x993 <68646455+x993@users.noreply.github.com>
2022-05-21 10:15:07 -0600

    TRYING

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import DataTable from 'react-data-table-component'
-import Card from '@material-ui/core/Card'
-import SortIcon from '@material-ui/icons/ArrowDownward'
-import Movies from './movies'
+import React from "react";
+import ReactDOM from "react-dom";
+import DataTable from "react-data-table-component";
+import Card from "@material-ui/core/Card";
+import SortIcon from "@material-ui/icons/ArrowDownward";
+import movies from "./movies";
 
 const columns = [
   {
@@ -28,20 +28,25 @@ const columns = [
     right: true,
     reorder: true
   }
-]
+];
 
-export default () => (
-  <div>
-    <Card>
-      <DataTable
-        title="Movies"
-        columns={columns}
-        data={Movies}
-        defaultSortFieldId={1}
-        sortIcon={<SortIcon />}
-        pagination
-        selectableRows
-      />
-    </Card>
-  </div>
-)
+function App() {
+  return (
+    <div className="App">
+      <Card>
+        <DataTable
+          title="Movies"
+          columns={columns}
+          data={movies}
+          defaultSortFieldId={1}
+          sortIcon={<SortIcon />}
+          pagination
+          selectableRows
+        />
+      </Card>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
