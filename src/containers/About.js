cb71b5ec8b9d191b0e94234774ecd5985b597561
x993 <68646455+x993@users.noreply.github.com>
2022-05-21 10:04:04 -0600

    tt

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from "react-dom";
-import DataTable from "react-data-table-component";
-import Card from "@material-ui/core/Card";
-import SortIcon from "@material-ui/icons/ArrowDownward";
-import movies from "./movies";
+import ReactDOM from 'react-dom'
+import DataTable from 'react-data-table-component'
+import Card from '@material-ui/core/Card'
+import SortIcon from '@material-ui/icons/ArrowDownward'
+import movies from './movies'
 
 const columns = [
   {
@@ -28,7 +28,7 @@ const columns = [
     right: true,
     reorder: true
   }
-];
+]
 
 return (
   <div className="table">
@@ -44,7 +44,7 @@ return (
       />
     </Card>
   </div>
-);
+)
 
 export default () => (
   <div>
